da4864c6995e65720b8217d150afe69986ed6ee2
matt-bowen <mt.bowen@hotmail.com>
2018-10-21 18:31:57 -0400

    Added qset, fix modal

--- assets/js/main.js
+++ assets/js/main.js
@@ -19,15 +19,29 @@
 		// Get the modal
 		var modal = document.getElementById('myModal');
 
+		var images = document.getElementsByClassName('myImages');
+
 		// Get the image and insert it inside the modal - use its "alt" text as a caption
-		var img = document.getElementById('myImg');
 		var modalImg = document.getElementById("img01");
 		var captionText = document.getElementById("caption");
-		img.onclick = function(){
-		    modal.style.display = "block";
-		    modalImg.src = this.src;
-		    captionText.innerHTML = this.alt;
+
+		for (var i = 0; i < images.length; i++) {
+			var img = images[i];
+		  	// and attach our click listener for this image.
+		  	img.onclick = function(evt) {
+		    	modal.style.display = "block";
+		    	modalImg.src = this.src;
+		    	captionText.innerHTML = this.alt;
+		    	evt.preventDefault();
+		  	}
 		}
+
+		//img.onclick = function($e){
+		//    modal.style.display = "block";
+		//   modalImg.src = this.src;
+		 //   captionText.innerHTML = this.alt;
+		 //   $e.preventDefault();
+		//}
 
 		// Get the <span> element that closes the modal
 		var span = document.getElementsByClassName("close")[0];
