ce8fbb52a94bd9bec1005552913e26351f671cfc
matt-bowen <mt.bowen@hotmail.com>
2019-11-26 00:14:59 -0500

    remove old images

--- assets/js/main.js
+++ assets/js/main.js
@@ -51,12 +51,12 @@
 		//}
 
 		// Get the <span> element that closes the modal
-		var span = document.getElementsByClassName("close")[0];
+		//var span = document.getElementsByClassName("close")[0];
 
 		// When the user clicks on <span> (x), close the modal
-		span.onclick = function() { 
-		  modal.style.display = "none";
-		}
+		//span.onclick = function() { 
+		//  modal.style.display = "none";
+		//}
 
 		
 
